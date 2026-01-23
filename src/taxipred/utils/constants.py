d7a7bc2462e4353c48ec653c25fddb94a2879cbe
Omer Aytug <aytugomer00@gmail.com>
2026-01-23 07:03:31 +0100

    path for the models dir

--- src/taxipred/utils/constants.py
+++ src/taxipred/utils/constants.py
@@ -3,3 +3,5 @@ from pathlib import Path
 DATA_PATH = Path(__file__).parents[1] / "data"
 TAXI_CSV_PATH = DATA_PATH / "taxi_trip_pricing.csv"
 NEW_CSV_PATH = DATA_PATH / "taxi_trip_pricing_cleaned.csv"
+
+MODEL_PATH = Path(__file__).parents[1] / "models"
